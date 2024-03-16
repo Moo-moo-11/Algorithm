3dee194d6a8cf353e453c20ae173b636e1fd9bc8
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-03-16 18:34:04 +0900

    Time: 183 ms (32.74%) | Memory: 36.8 MB (63.79%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.kt
+++ 14-longest-common-prefix/longest-common-prefix.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun longestCommonPrefix(strs: Array<String>): String {
 
-        var a = 201
+        var a = strs[0].length
         for (item in strs) {
             if (a > item.length) {
                 a = item.length
@@ -10,24 +10,15 @@ class Solution {
 
         var pre = ""
 
-        if (a == 0) {
-            return pre
-        } else {
-
-            loop1@ for (i in 0..a - 1) {
-
-                val c = strs[0].substring(0, i + 1)
-
-                for (item in strs) {
-                    if (c != item.substring(0, i + 1)) {
-                        break@loop1
-                    }
+        loop@ for (i in 0..a - 1) {
+            val c = strs[0].substring(0, i + 1)
+            for (item in strs) {
+                if (c != item.substring(0, i + 1)) {
+                    break@loop
                 }
-
-                pre = c
             }
-
-            return pre
+            pre = c
         }
+        return pre
     }
 }
