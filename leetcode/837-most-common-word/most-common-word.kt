1976a43aff1a7d211f6bbae4b71279238dc23c18
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-05-03 11:17:05 +0900

    Time: 218 ms (71.83%) | Memory: 40.8 MB (14.08%) - LeetSync

---
+++ leetcode/837-most-common-word/most-common-word.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun mostCommonWord(paragraph: String, banned: Array<String>): String {
+        val counts:MutableMap<String,Int> = mutableMapOf()
+
+        val words = paragraph.replace("""\W+""".toRegex(), " ").lowercase().trim().split(" ")
+        for (word in words) {
+            if (!banned.contains(word)) {
+                counts[word] = counts.getOrDefault(word,0)+1
+            }
+        }
+        
+        return counts.maxBy { it.value }.key
+    }
+}
