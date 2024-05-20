fd13775556e8a0284bed8e3631b809af4aec2539
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-05-20 10:47:56 +0900

    Time: 430 ms (93.38%) | Memory: 54.3 MB (83.25%) - LeetSync

---
+++ leetcode/15-3sum/3sum.kt
@@ -0,0 +1,37 @@
+class Solution {
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        var left: Int
+        var right: Int
+        var sum: Int
+        val results: MutableList<List<Int>> = mutableListOf()
+
+        nums.sort()
+
+        for(i in 0..nums.size-3) {
+            if (i > 0 && nums[i] == nums[i-1]) continue
+            
+            left = i + 1
+            right = nums.size - 1
+
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right]
+
+                if (sum < 0) left += 1
+                else if (sum > 0) right -= 1
+                else {
+                    results.add(listOf(nums[i],nums[left],nums[right]))
+
+                    while (left<right && nums[left] == nums[left + 1]) left += 1
+                    while (left<right && nums[right] == nums[right - 1]) right -= 1
+
+                    left += 1
+                    right -= 1
+                }
+
+            }
+        }
+        
+        return results
+        
+    }
+}
