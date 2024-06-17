410655807dcc272919b7524bc1f5ae0fce8ddd0e
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-06-17 22:55:48 +0900

    Time: 173 ms (26.43%) | Memory: 34.2 MB (79.27%) - LeetSync

--- leetcode/782-jewels-and-stones/jewels-and-stones.kt
+++ leetcode/782-jewels-and-stones/jewels-and-stones.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun numJewelsInStones(jewels: String, stones: String): Int {
         var answer = 0
-        val set = mutableSetOf<Char>()
+        val set = hashSetOf<Char>()
 
         for (jewel in jewels) 
         set.add(jewel)
