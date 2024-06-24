a9831d82f0affc6c8b402ac1b5fbb1b3bf10497d
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-06-24 15:26:59 +0900

    Time: 258 ms (81.37%) | Memory: 40.7 MB (94.78%) - LeetSync

--- leetcode/347-top-k-frequent-elements/top-k-frequent-elements.kt
+++ leetcode/347-top-k-frequent-elements/top-k-frequent-elements.kt
@@ -6,17 +6,17 @@ class Solution {
             map[n] = map.getOrDefault(n, 0) + 1
         }
 
-        val priorityQueue: PriorityQueue<IntArray> = PriorityQueue { a, b -> b[1] - a[1] }
+        val priorityQueue = PriorityQueue<Pair<Int,Int>> { a, b -> b.second - a.second }
 
         for (key in map.keys) {
-            priorityQueue.add(intArrayOf(key, map[key]!!))
+            priorityQueue.add(key to map[key]!!)
         }
 
         val answer = IntArray(k)
         for (i in 0..<k) {
-            answer[i] = priorityQueue.poll()[0]
+            answer[i] = priorityQueue.poll().first
         }
 
-        return answer    
+        return answer
     }
 }
