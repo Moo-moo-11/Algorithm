aa45f0affa68258029c81bfd7ddbdd084f6dff0e
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-06-27 09:24:56 +0900

    Time: 1260 ms (77.42%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/1268-market-analysis-i/market-analysis-i.sql
+++ leetcode/1268-market-analysis-i/market-analysis-i.sql
@@ -1,3 +1,3 @@
 SELECT a.user_id buyer_id, a.join_date, COUNT(order_id) orders_in_2019
-FROM Users a LEFT JOIN (SELECT * FROM Orders WHERE YEAR(order_date) = 2019) b ON a.user_id = b.buyer_id
+FROM Users a LEFT JOIN Orders b ON a.user_id = b.buyer_id AND YEAR(b.order_date) = 2019
 GROUP BY 1, 2
